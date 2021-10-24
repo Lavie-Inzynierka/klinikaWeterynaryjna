6b6ffb445902cec1106435f33927fd0c35a61b54
LaVie1502 <laviee1502@gmail.com>
2021-10-24 13:17:23 +0200

    Struktura bazy danych

--- bazaDanych/tabele.sql
+++ bazaDanych/tabele.sql
@@ -1,3 +1,4 @@
 create schema if not exists klinika;
 set search_path = 'klinika';
 
+--create table if not exists
--- klinikaWeterynaryjna/settings.py
+++ klinikaWeterynaryjna/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import bazaDanych.privdb as pv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-qx+!2owtmnu+!8x)9561hfbuift=0oe@d2a$=sqbclhkdq+4h@
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,8 +53,7 @@ ROOT_URLCONF = 'klinikaWeterynaryjna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,17 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'klinikaWeterynaryjna.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': pv.nazwa,
+        'USER': pv.uzytkownik,
+        'PASSWORD': pv.haslo,
+        'HOST': pv.host,
+        'PORT': pv.port,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -113,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
