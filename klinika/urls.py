0d38eb44678705fbf6ba97455e76f5790d96334b
LaVie1502 <laviee1502@gmail.com>
2021-11-04 16:03:33 +0100

    Poprawiona metoda przejscia

--- klinika/urls.py
+++ klinika/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
-    path('', views.stronaGlowna, name="Vet Pet"),
-    path('informacje/', views.informacje, name="o nas"),
-    path('logowanie/', views.logowanie, name="zaloguj się"),
-    path('rejestracja/', views.rejestracja, name="zarejestruj się"),
+    path('', views.stronaGlowna, name="VetPet"),
+    path('informacje/', views.informacje, name="info"),
+    path('logowanie/', views.logowanie, name="logowanie"),
+    path('rejestracja/', views.rejestracja, name="rejestracja"),
 ]
