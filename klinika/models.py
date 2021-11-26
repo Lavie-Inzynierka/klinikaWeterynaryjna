6a109652d60982acf57dd3b06d89f30b2a86385d
LaVie1502 <laviee1502@gmail.com>
2021-11-26 20:49:29 +0100

    django models

--- klinika/models.py
+++ klinika/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from datetime import datetime, timedelta, date
 import uuid
+from django.contrib.auth.models import User
+from django.db import models
 
 
 # Create your models here.
@@ -12,10 +14,46 @@ class Species(models.Model):
     def __str__(self):
         return self.species_name
 
-# todo: class permission -> id, permission name (admin, pet_owner, vet, receptionist)
-#       user_permission -> user_id, permission_id
-#       class pet, owner, pet_owner
 
+class Token(models.Model):
+    id = models.AutoField(default=0, unique=True, primary_key=True, editable=False)
+    token = models.CharField(max_length=250, unique=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    expires_at = models.DateTimeField(default=datetime.now() + timedelta(days=14))
+
+    is_active = models.BooleanField(default=True)
+
+    def expires(self):
+        if datetime.now() > self.expires_at:
+            self.is_active = False
+
+
+class UserToken(models.Model):
+    id = models.AutoField(default=0, unique=True, primary_key=True, editable=False)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    token_id = models.OneToOneField(Token, on_delete=models.CASCADE)
+
+
+PERMISSION_CHOICES = [
+    ['AD', 'Admin'],
+    ['VE', 'Vet'],
+    ['RC', 'Receptionist'],
+    ['OW', 'Pet owner'],
+]
+
+
+class UserTypeEnum(object):
+    ADMIN = 'AD'
+    VET = 'VE'
+    RECEPTIONIST = 'RC'
+    PET_OWNER = 'OW'
+
+
+class Permission(models.Model):
+    id = models.AutoField(default=0, unique=True, primary_key=True, editable=False)
+    permission_name = models.CharField(max_length=30, choices=PERMISSION_CHOICES, default=UserTypeEnum.PET_OWNER)
+
+# todo:   class pet, owner, pet_owner
 # class Zwierze(models.Model):
 #     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 #     nazwa = models.CharField(max_length=200)
