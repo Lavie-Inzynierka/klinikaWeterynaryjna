05759915300f6c1026b7395cb146f1bd5c34c165
LaVie1502 <laviee1502@gmail.com>
2021-12-01 22:18:57 +0100

    sciezka aktywacyjna

--- klinika/urls.py
+++ klinika/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
+    path('activate/<token>/', views.VerificationView, name="activate"),
 ]
