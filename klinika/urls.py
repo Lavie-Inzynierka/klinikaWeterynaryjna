5897374a75297496765092f50641e1928231ac78
LaVie1502 <laviee1502@gmail.com>
2021-12-13 16:14:40 +0100

    quick fix

--- klinika/urls.py
+++ klinika/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name="VetPet"),
-    path('about', views.about, name="about"),
-    path('signin', views.signin, name="signin"),
-    path('signup', views.signup, name="signup"),
-    path('signout', views.signout, name="signout"),
-    path('mypets', views.mypets, name="mypets"),
-    path('mypet/<petid>', views.mypet, name="mypet"),
-    path('addpet', views.addpet, name="addpet"),
-    path('activate/<token>', views.VerificationView, name="activate"),
+    path('about/', views.about, name="about"),
+    path('signin/', views.signin, name="signin"),
+    path('signup/', views.signup, name="signup"),
+    path('signout/', views.signout, name="signout"),
+    path('mypets/', views.mypets, name="mypets"),
+    path('mypet/<petid>/', views.mypet, name="mypet"),
+    path('addpet/', views.addpet, name="addpet"),
+    path('activate/<token>/', views.VerificationView, name="activate"),
 ]
