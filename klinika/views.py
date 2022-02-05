241006dd3299ef84b2ceee2f9d58d41e63fa935b
Laviee <Laviee1502@gmail.com>
2022-02-06 00:28:27 +0100

    pobranie modelu właściciela oraz adresow użytkownika

--- klinika/views.py
+++ klinika/views.py
@@ -3,7 +3,7 @@ import datetime
 import os
 import bleach
 from django.contrib import messages
-from .models import MyUser, Pet, Gender_choices, UserType, UserTypeEnum, Species
+from .models import MyUser, Pet, Gender_choices, UserType, UserTypeEnum, Species, Owner, UserAddresses
 from django.shortcuts import render, redirect
 from sendgrid import SendGridAPIClient
 import bcrypt
