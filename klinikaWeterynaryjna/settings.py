7bf287a2a97c94793816e3726488145223dc6fb0
Laviee <Laviee1502@gmail.com>
2022-02-19 19:18:50 +0100

    usunięcie django admina

--- klinikaWeterynaryjna/settings.py
+++ klinikaWeterynaryjna/settings.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ["*"
 
 INSTALLED_APPS = [
     'klinika.apps.KlinikaConfig',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
